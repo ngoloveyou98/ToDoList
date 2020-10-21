706f495852ceb06e37eca3b8eb61980af964d581
thuctapgit <vinhnt62@wru.vn>
2020-10-21 17:29:00 +0700

    funtion add, edit, remove

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="work" >
-    <b-form-checkbox 
-    size="lg" 
-    v-model="dowork.status"
-    class="work-checkbox"
-    @change="selectWork()"
-    >
-    {{dowork.content}}
-    </b-form-checkbox>
+    <div class="custom-control custom-checkbox todo-list" >
+      <input
+        type="checkbox"
+        class="custom-control-input work-checkbox"
+        :id="dowork.id"
+        :checked="dowork.status"
+        @change="selectWork()"
+      />
 
+      <label  class="custom-control-label" 
+        :for="dowork.id"
+        >
+        <div v-if="!dowork.editing" @dblclick="editTodo(dowork.id)" :class="{complete: dowork.status}">
+          {{ dowork.content }}
+        </div>
+        <div v-else>
+          <input v-focus type="text" :value="dowork.content" 
+          @keyup.enter="changeContent(dowork, $event)" 
+          @keyup.esc ="cancelContent(beforEditCache, dowork.id)"
+          class="input-hover"
+          />
+        </div>
+      </label>
+      <b-icon icon="x-circle" scale="2" variant class="icon" @click="removeTodo(dowork.id)"></b-icon>
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      beforEditCache:''
+    }
+  },
   name: "work",
   props: {
     dowork: {
@@ -23,20 +45,85 @@ export default {
       }
     }
   },
-  methods:{
-      selectWork(){
-          console.log("event click")
-          var data = {
-                id:this.dowork.id,
-                content : this.dowork.content,
-                status : this.dowork.status
-          }
-          this.$emit('changeStatus',data)
+  directives: {
+    focus: {
+      focus: {
+      // directive definition
+        mounted(el) {
+          el.focus()
+        }
       }
-  }
+    }
+  },
+  methods: {
+    selectWork() {
+      // console.log("event click");
+      var data = {
+        id: this.dowork.id,
+        content: this.dowork.content,
+        status: this.dowork.status
+      };
+      this.$emit("changeStatus", data);
+    },
+    removeTodo(id) {
+      this.$emit("remoteTodo", id);
+    },
+    editTodo(id){
+      this.beforEditCache = this.dowork.content
+      
+      this.$emit('editTodo',id)
+    },
+    changeContent(dowork, $event){
+      if($event.target.value.trim() == ''){
+        return
+      }
+      var dataContent = {
+        id: dowork.id,
+        content: $event.target.value
+      }
+      this.$emit('changeContent',dataContent)
+    },
+    cancelContent(beforEditCache,id){
+      var dataCancel ={
+        id: id,dataCancel,
+        content: beforEditCache
+      }
+      this.$emit('cancelContent',dataCancel)
+    }
+  },
+  
 };
 </script>
 
-<style>
+<style lang="scss">
+.work {
+  margin-top: 10px;
+  font-size: 20px;
+}
+.work-checkbox {
+  // font-size: 24px;
+  margin-top: 10px;
+}
+.icon {
+  cursor: pointer;
+  float: right;
+}
+.todo-editing {
+  margin-left: 100px;
+}
+.complete{
+  text-decoration: line-through;
+  color: gray;
+}
+.todo-list{
+  background: blanchedalmond;
+  border-radius: 10px;
+  padding: 10px;
+}
+.input-hover{
+  :hover{
+    background-color: white;
+  }
+}
+</style>>
 
-</style>
--- src/components/WorkList.vue
+++ src/components/WorkList.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="list-user">
     <h1>List Work</h1>
-    <work v-for="work in workOB"
+    <input type="text" class="todo-input" 
+    placeholder="What needs to be done" 
+    @keyup.enter="addWork()" 
+    v-model="newTodo">
+    <work v-for="work in workOB "
             :key="work.id"
             v-bind:dowork="work"
             v-on:changeStatus="changeStatusWork"
+            v-on:remoteTodo="remoteTodo"
+            v-on:editTodo="editTodo"
+            v-on:changeContent="changeContent"
+            v-on:cancelContent="cancelContent"
+            class="hover"
     />
     
   </div>
@@ -13,10 +22,16 @@
 <script>
 import work from "./Work";
 export default {
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 4
+    }
+  },
   name: "work-list",
   props: {
     workOB: {
-      type: Object,
+      type: Array,
       default() {
         return [];
       }
@@ -27,17 +42,61 @@ export default {
   },
   methods:{
       changeStatusWork(data){
-          console.log(data.content)
+          var id = data.id
+          this.$emit('changeStatus',id)
+      },
+      addWork(){
+        if(this.newTodo.trim().length == 0){
+          alert('aaa')
+          return
+        }
+        this.workOB.push({
+            id: this.idForTodo,
+            content: this.newTodo,
+            status:false
+        });
+        this.idForTodo++
+        this.newTodo= ''
+      },
+      remoteTodo(id){
+        var idTodo = id
+        this.$emit('remoteTodo',idTodo)
+      },
+      editTodo(id){
+          this.$emit('editTodo',id)
+      },
+      changeContent(dataContent){
+        this.$emit('changeContent',dataContent)
+      },
+      cancelContent(dataCancel){
+        this.$emit('cancelContent',dataCancel)
       }
-  }
+      
+  },
+ 
 };
 </script>
 
-<style>
-.list-user{
-    height: 500px;
-    background-color: blanchedalmond;
-    width: 500px;
-    margin-left: 600px ;
-}
-</style>
+<style lang="scss">
+  .list-user{
+      background-color: white;
+      max-width: 500px;
+      margin-left: 500px ;
+      border: 50px;
+  }
+  .todo-input{
+    width: 100%;
+    padding: 8px 18px;
+    border: 1px solid blue; 
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-size: 20px;
+  }
+  .hover{
+    :hover{
+      background-color: burlywood;;
+    }
+  }
+
+</style>>
+
